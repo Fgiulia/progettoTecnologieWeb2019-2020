4cd943d011e1b51dc66050c57e275a4f4a32470a
Fgiulia <fioresegiulia@hotmail.com>
2019-12-12 17:06:13 +0100

    add moduleseInit.php

---
+++ PHP/modulesInit.php
@@ -0,0 +1,20 @@
+<?php
+    class modulesInit{
+        #funzione per scrivere i breadcrumb
+        public static function breadcrumb(...$sequenza){
+            $breadcrumb = "Ti trovi in: Home ";
+            foreach($sequenza as $element){
+                $breadcrumb .= "> $element ";
+            }
+            return "<p id=\"map-position\">$breadcrumb</p>";
+        }
+
+        #funzione per creare il menu'
+        public static function menu(){
+            if(!isset($_SESSION)) {
+                session_start();
+            }
+            $menu=
+        }
+    }
+?>
