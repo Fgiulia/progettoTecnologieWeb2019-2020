4fae14de6bbc7fa2de1ada0a1c3d40f3f324de9b
Fgiulia <fioresegiulia@hotmail.com>
2019-12-17 12:28:07 +0100

    add menu

--- PHP/modulesInit.php
+++ PHP/modulesInit.php
@@ -14,7 +14,44 @@
             if(!isset($_SESSION)) {
                 session_start();
             }
-            
+
+            $menu_form = '<div id="menu">'."\n";
+            $menu_form .= '<a href="">Home</a>'."\n";
+            $menu_form .= '<div class="closeMenu">'."\n";
+            $menu_form .= '<div id="chiudiMenu">'."\n";
+            $menu_form .= '</div>'."\n";
+            $menu_form .= '</div>'."\n";
+            $menu_form .= '<div class="dropdown">'."\n";
+            $menu_form .= '<button class="dropbtn">Animali<i class="fa fa-caret-down"></i></button>'."\n";
+            $menu_form .= '<div class="dropdown-content">'."\n";
+            $menu_form .= '<a href="">Tutti gli animali</a>'."\n";
+            $menu_form .= '<a href="">I cuccioli</a>'."\n";
+            $menu_form .= '</div>'."\n";
+            $menu_form .= '</div>'."\n";
+            $menu_form .= '<a href="">Eventi</a>'."\n";
+            $menu_form .= '<div class="dropdown">'."\n";
+            $menu_form .= '<button class="dropbtn"><a href="">Informazioni</a><i class="fa fa-caret-down"></i></button>'."\n";
+            $menu_form .= '<div class="dropdown-content">'."\n";
+            $menu_form .= '<a href="">Contattaci</a>'."\n";
+            $menu_form .= '</div>'."\n";
+            $menu_form .= '</div>'."\n";
+            $menu_form .= '<a href="">Acquista</a>'."\n";
+            #gestione di accedi/area personale
+            #if(isset($_SESSION['email']) && $_SESSION['email']=="admin@admin.com"){
+                #$menu_form .= '<a href=""></a>'."\n";
+            #}
+            #else{
+                #if(isset($_SESSION)){
+                    #$menu_form .= '<a href=""></a>'."\n";
+                #}
+                #else{
+                    #$menu_form .= '<a href=""></a>'."\n";
+                #}
+            #}
+            $menu_form .= '<a href="javascript:void(0);" class="icon" onclick="myFunction()">'."\n";
+            $menu_form .= '</div>'."\n";
+
+            return $menu_form;
         }
     }
 ?>
