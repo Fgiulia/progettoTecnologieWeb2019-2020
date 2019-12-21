69a6bb67c07d34e3114dcfef196fce3cd4a41054
TheJack95 <giacomocorro@gmail.com>
2019-12-21 12:40:52 +0100

    fix css menu

--- PHP/modulesInit.php
+++ PHP/modulesInit.php
@@ -6,7 +6,7 @@
             foreach($sequenza as $element){
                 $breadcrumb .= "> $element ";
             }
-            return "<p id=\"map-position\">$breadcrumb</p>";
+            return "<p class=\"map-position\">$breadcrumb</p>";
         }
 
         #funzione per creare il menu'
@@ -16,30 +16,30 @@
             }
 
             $menu_form = '<div id="menu">'."\n"
-                        .'  <a href="home.php">Home</a>'."\n"
+                        .'  <a class="menuItem" href="home.php">Home</a>'."\n"
                         .'  <div class="closeMenu">'."\n"
                         .'      <div id="chiudiMenu">'."\n"
                         .'      </div>'."\n"
                         .'  </div>'."\n"
-                        .'  <div class="dropdown">'."\n"
-                        .'      <button class="dropbtn">Animali<i class="fa fa-caret-down"></i></button>'."\n"
+                        .'  <div class="dropdown menuItem">'."\n"
+                        .'      <a href="" class="dropbtn">Animali</a>'."\n"
                         .'      <div class="dropdown-content">'."\n"
                         .'          <a href="animali.php">Tutti gli animali</a>'."\n"
                         .'          <a href="cuccioli.php">I cuccioli</a>'."\n"
                         .'      </div>'."\n"
                         .'  </div>'."\n"
-                        .'  <a href="">Eventi</a>'."\n"
-                        .'  <div class="dropdown">'."\n"
-                        .'      <button class="dropbtn">Informazioni<i class="fa fa-caret-down"></i></button>'."\n"
+                        .'  <a class="menuItem" href="">Eventi</a>'."\n"
+                        .'  <div class="dropdown menuItem">'."\n"
+                        .'      <a href="" class="dropbtn">Informazioni</a>'."\n"
                         .'      <div class="dropdown-content">'."\n"
                         .'          <a href="">Contattaci</a>'."\n"
                         .'      </div>'."\n"
                         .'  </div>'."\n"
-                        .'  <a href="">Acquista</a>'."\n";
+                        .'  <a class="menuItem" href="">Acquista</a>'."\n";
             #gestione di accedi area personale
             if(isset($_SESSION['admin']) && $_SESSION['admin'] == 1){
-                $menu_form .= '<div class="dropdown">'."\n"
-                             .'   <button class="dropbtn">Area Personale<i class="fa fa-caret-down"></i></button>'."\n"
+                $menu_form .= '<div class="dropdown menuItem">'."\n"
+                             .'   <a href="" class="dropbtn">Area Personale</a>'."\n"
                              .'   <div class="dropdown-content">'."\n"
                              .'      <a href="areaPrivata.php">Pannello Amministrativo</a>'."\n"
                              .'      <a href="">Logout</a>'."\n"
@@ -48,8 +48,8 @@
             }
             else{
                 if(isset($_SESSION["logged"]) && $_SESSION["logged"]->status == 2) { //login effettuato correttamente
-                    $menu_form .= '<div class="dropdown">'."\n"
-                                 .'   <button class="dropbtn"><a href="">Area Personale</a><i class="fa fa-caret-down"></i></button>'."\n"
+                    $menu_form .= '<div class="dropdown menuItem">'."\n"
+                                 .'   <a href="" class="dropbtn"><a href="">Area Personale</a></a>'."\n"
                                  .'   <div class="dropdown-content">'."\n"
                                  .'      <a href="areaPrivata.php">I tuoi acquisti</a>'."\n"
                                  .'      <a href="">Modifica i tuoi dati</a>'."\n"
@@ -58,7 +58,7 @@
                                  .'</div>'."\n";
                 }
                 else{
-                    $menu_form .= '   <a href="login.php">Accedi</a>'."\n";
+                    $menu_form .= '   <a class="menuItem" href="login.php">Accedi</a>'."\n";
                 }
             }
             $menu_form .= '   <a href="javascript:void(0);" class="icon" onclick="myFunction()"></a>'."\n";
