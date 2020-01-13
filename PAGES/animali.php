5890f1e1dd571fee21325454f1fd69c1454535a6
Giulia <fioresegiulia@hotmail.com>
2020-01-13 18:33:32 +0100

    fixed error

--- PAGES/animali.php
+++ PAGES/animali.php
@@ -2,17 +2,21 @@
 require_once "../PHP/modulesInit.php";
 require_once "../PHP/sqlInteractios.php";
 
+if((isset($_POST['cerca']) || isset($_POST['scegliFamiglia']))!=true){
+    $_POST['cerca']=null;
+    $_POST['scegliFamiglia']=null;
+}
+
 $output = file_get_contents("../HTML/animali.html");
 $output = str_replace('<a href="animali.php">','</a>',$output);
 $output = str_replace("<menu></menu>",modulesInit::menu(),$output);
 $output = str_replace("<breadcrumb></breadcrumb>",modulesInit::breadcrumb('Animali > Tutti gli animali'),$output);
 
-$cercaTesto = $_POST['cerca'];
-
 $oggettoPagina = new sqlInteractions();
 $connessione = $oggettoPagina->apriConnessioneDB();
 
 if($connessione){
+    $cercaTesto = $_POST['cerca'];
     if($cercaTesto!=null){
         $animali = $oggettoPagina->getSelect($cercaTesto);
     }
@@ -30,12 +34,13 @@ if($connessione){
         $stringaAnimali = "<p>Non abbiamo trovato nessun animale collegato alla tua ricerca.</p><p>Pu&ograve; essere che non siano presenti gli animali che cerchi in questo momento al Parco Faunistico Euganeo.</p>";
     }
     else{
+        $stringaAnimali = "<p></p>";
         if($cercaTesto!=null){
-            $stringaAnimali = "<p>Risultati per \"".$cercaTesto."\"</p>";
+            $stringaAnimali .= "<p>Risultati per \"".$cercaTesto."\"</p>";
         }
         $stringaAnimali .= "<dl id=\"risultatiAnimali\">";
         foreach($animali as $animals){
-			$stringaAnimali .= "<dt>".$animals['Ritratto']."</dt><dt>".$puppies['NomeComune']."</dt><dt>".$puppies['NomeScientifico']."</dt>";
+			$stringaAnimali .= "<dt>".$animals['Ritratto']."</dt><dt>".$animals['NomeComune']."</dt><dt>".$animals['NomeScientifico']."</dt>";
 			}
 			$stringaAnimali .= "</dl>";
     }
--- PHP/sqlInteractios.php
+++ PHP/sqlInteractios.php
@@ -24,9 +24,9 @@ class sqlInteractions {
     
 #funzione per la lettura da DB dei cuccioli
 	public function getCuccioli(){
-		$query = "SELECT NomeComune, NomeProprio, Ritratto FROM Animali WHERE Famiglia=\'Cuccioli\' ORDER BY NomeComune ASC";
+		$query = 'SELECT NomeComune, NomeProprio, Ritratto FROM Animali WHERE Famiglia=\'Cuccioli\' ORDER BY NomeComune ASC';
 		$queryResult = mysqli_query($this->connection,$query);
-		
+
 		if(mysqli_num_rows($queryResult)==0){
 			return null;
 		}
@@ -43,10 +43,10 @@ class sqlInteractions {
 #funzione per la lettura da DB con passaggio del nome (ricerca da input)
 	public function getSelect($testo) {
 		if($testo==null){
-			$select = "SELECT NomeComune, NomeScientifico, Ritratto FROM Animali WHERE Famiglia!=\'Cuccioli\' ORDER BY NomeComune ASC";
+			$select = 'SELECT NomeComune, NomeScientifico, Ritratto FROM Animali WHERE Famiglia!=\'Cuccioli\' ORDER BY NomeComune ASC';
 		}
 		else{
-			$select = "SELECT NomeComune, NomeScientifico, Ritratto FROM Animali WHERE NomeComune=\'.$testo.\'OR NomeScientifico=\'.$testo.\' OR Famiglia=\'.$testo.\' OR SezioneParco=\'.$testo.\' ORDER BY NomeComune ASC";
+			$select = 'SELECT NomeComune, NomeScientifico, Ritratto FROM Animali WHERE NomeComune=\'.$testo.\'OR NomeScientifico=\''.$testo.'\' OR Famiglia=\''.$testo.'\' OR SezioneParco=\''.$testo.'\' ORDER BY NomeComune ASC';
 		}
 		$selectResult = mysqli_query($this->connection,$select);
 
@@ -66,20 +66,20 @@ class sqlInteractions {
 #funzione per la lettura da DB con passaggio della famiglia (menu a tendina)
     public function getFamily($famiglia) {
         if($famiglia==null){
-            $family="SELECT NomeComune, NomeScientifico, Ritratto FROM Animali WHERE Famiglia!=\'Cuccioli\' ORDER BY NomeComune ASC";
+            $family = 'SELECT NomeComune, NomeScientifico, Ritratto FROM Animali WHERE Famiglia!=\'Cuccioli\' ORDER BY NomeComune ASC';
         }
         else{
 			if($famiglia=='animali'){
-                $family = "SELECT NomeComune, NomeScientifico, Ritratto FROM Animali WHERE Famiglia!=\'Cuccioli\' ORDER BY NomeComune ASC";
+                $family = 'SELECT NomeComune, NomeScientifico, Ritratto FROM Animali WHERE Famiglia!=\'Cuccioli\' ORDER BY NomeComune ASC';
             }
             if($famiglia=='mammiferi'){
-                $family = "SELECT NomeComune, NomeScientifico, Ritratto FROM Animali WHERE Famiglia=\'Mammiferi\' ORDER BY NomeComune ASC";
+                $family = 'SELECT NomeComune, NomeScientifico, Ritratto FROM Animali WHERE Famiglia=\'Mammiferi\' ORDER BY NomeComune ASC';
             }
             if($famiglia=='rettili'){
-                $family = "SELECT NomeComune, NomeScientifico, Ritratto FROM Animali WHERE Famiglia=\'Rettili\' ORDER BY NomeComune ASC";
+                $family = 'SELECT NomeComune, NomeScientifico, Ritratto FROM Animali WHERE Famiglia=\'Rettili\' ORDER BY NomeComune ASC';
             }
             if($famiglia=='uccelli'){
-                $family = "SELECT NomeComune, NomeScientifico, Ritratto FROM Animali WHERE Famiglia=\'Uccelli\' ORDER BY NomeComune ASC";
+                $family = 'SELECT NomeComune, NomeScientifico, Ritratto FROM Animali WHERE Famiglia=\'Uccelli\' ORDER BY NomeComune ASC';
             }
         }
         $familyResult = mysqli_query($this->connection,$family);
