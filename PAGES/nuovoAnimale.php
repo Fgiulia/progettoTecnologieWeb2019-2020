22e00c50eaeddf8ef53b0c30f7844165de134d9d
Giulia <fioresegiulia@hotmail.com>
2020-01-16 10:49:32 +0100

    modifica database

--- PAGES/nuovoAnimale.php
+++ PAGES/nuovoAnimale.php
@@ -7,19 +7,24 @@ if(!isset($_SESSION))
 if(isset($_SESSION["logged"]) && $_SESSION["logged"]->status == 2) {
     $output = file_get_contents("../HTML/nuovoAnimale.html");
     $output = str_replace("<menu></menu>",modulesInit::menu(),$output);
-    $output = str_replace("<breadcrumb></breadcrumb>",modulesInit::breadcrumb("Pannello amministratore > Animali"),$output);
+    $output = str_replace("<breadcrumb></breadcrumb>",modulesInit::breadcrumb("Pannello amministratore >> Animali"),$output);
+    
+    if(isset($_POST['nomeComune']) && isset($_POST['nomeScientifico'])){
+        $nuovoAnimale = "messagggio";
+        $output = str_replace("<inserimentoNuovoAnimale></inserimentoNuovoAnimale>",$nuovoAnimale,$output);
+    }
 
     echo $output;
 }
 else {
     $response = (Object) [
 		"status" => -1
-		,"response" => "Attenzione: non hai effettuato il login&comma; verrai reindirizzato alla pagina di login&period;"
+		,"response" => "Attenzione&colon; non hai effettuato il login&comma; verrai reindirizzato alla pagina di login&period;"
 	];
 	$_SESSION["logged"] = $response;
 
 	echo modulesInit::setMessaggio($response->response, true);
 
-    header("refresh:3; url= http://localhost:8080/progettoTecnologieWeb2019-2020/PAGES/login.php");
+    header("refresh:5; url= http://localhost:8080/progettoTecnologieWeb2019-2020/PAGES/login.php");
 }
 ?>
