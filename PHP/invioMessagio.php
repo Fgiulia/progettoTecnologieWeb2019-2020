159c8faf7886f7b898d8ac64648eb3c440bb658b
Erik Tabacchi <tabacchi.erik@gmail.com>
2020-01-16 15:16:33 +0100

    Sistemato invioMessaggio.php

--- PHP/invioMessagio.php
+++ PHP/invioMessagio.php
@@ -1,19 +1,39 @@
 <?php
 
-$nome = $_POST["nome"];
-$cognome = $_POST["cognome"];
-$email = $_POST["email"];
-$telefono = $_POST["numerotelefono"];
-$messaggio = $_POST["messaggio"]
+require("config/config.php");
+require("api/fnQuery.php");
 
-$oggettoPagina = new sqlInteractions();
-$connessione = $oggettoPagina->apriConnessioneDB();
+$response = (Object) [
+	"status" => false
+	,"response" => "init"
+];
 
-if($connessione)
-{
+
+
+    $nome = $_POST["nome"];
+    $cognome = $_POST["cognome"];
+    $email = $_POST["email"];
+    $telefono = $_POST["numeroTelefono"];
+    $messaggio = $_POST["messaggio"];
+    
+    if ($dbh) {
+        
+        $params = [$nome, $cognome, $email, $telefono, $messaggio];
+        $sql = "INSERT INTO messaggi (Nome,Cognome,Email,NumeroTelefono,Messaggio) 
+                VALUES (?,?,?,?,?)";
+        $query = query($dbh, $sql, $params);
+
+        if ($query->status) {
+            $response->response = "Messaggio inviato";
+            $response->status = true;
+        } else  {
+            $response->response = "Si è verificato un errore durante l'invio";
+        }
+    }
+    else
+        echo "impossibile connetersi al database";
+    
+    echo $response->response;
     
 
-
-
-
-}
+    ?>
