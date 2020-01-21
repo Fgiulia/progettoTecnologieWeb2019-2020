329eeb7bec75f987d6e50f413678af95b87bff3d
Giulia <fioresegiulia@hotmail.com>
2020-01-21 14:32:26 +0100

    aggiunti controlli input php

--- PAGES/nuovoAnimale.php
+++ PAGES/nuovoAnimale.php
@@ -8,51 +8,74 @@
     if(isset($_SESSION["logged"]) && $_SESSION["logged"]->status == 2) {
         $output = file_get_contents("../HTML/nuovoAnimale.html");
         $output = str_replace("<menu></menu>",modulesInit::menu(),$output);
-        $output = str_replace("<breadcrumb></breadcrumb>",modulesInit::breadcrumb("Pannello Amministratore &gt;&gt; Inserimento Nuovo Animale"),$output);
+        $output = str_replace("<breadcrumb></breadcrumb>",modulesInit::breadcrumb("Pannello Amministratore >> Inserimento Nuovo Animale"),$output);
  
  #controllo se i campi obbligatori sono stati inseriti e se sono validi
         if(isset($_POST['nomeComune'])){
-            if(preg_match("/^[a-zA-Z ]*$/",$_POST['nomeComune'])){
-                if(isset($_POST['nomeScientifico'])){
-                    if(preg_match("/^[a-zA-Z ]*$/",$_POST['nomeScientifico'])){
-                        if(isset($_POST['famiglia'])){
-                            if(preg_match("/^[a-zA-Z ]*$/",$_POST['famiglia'])){
-                                $oggettoPagina = new sqlInteractions();
-                                $connessione = $oggettoPagina->apriConnessioneDB();
-                                if($connessione){
-                                    $inserimento = $oggettoPagina->insertAnimal();
-                                    if($inserimento){
-                                        $nuovoAnimale = "Inserimento nuovo animale avvenuto con successo&period;";
-                                    }
-                                    else{
-                                        $nuovoAnimale = "Inserimento nuovo animale non avvenuto per un problema del database&period;";
-                                    }
-                                }
-                                else{
-                                    $nuovoAnimale = "Connessione al database degli animali fallita&period; Non &egrave; possibile procedere con l&apos;inserimento&comma; riprova pi&ugrave; tardi&period;";
-                                }
+            if(isset($_POST['nomeScientifico']) && isset($_POST['famiglia'])){
+                if(modulesInit::validName($_POST['nomeComune']) && modulesInit::validName($_POST['nomeScientifico'])){
+                    $valid = true;
+                    if(isset($_POST['nomeProprio'])){
+                        if(modulesInit::validName($_POST['nomeProprio'])){
+                            $valid = true;
+                        }
+                        else{
+                            $valid = false;
+                        }
+                    }
+                    if(isset($_POST['descrizioneImmagine']) && $valid==true){
+                        if(modulesInit::validName($_POST['descrizioneImmagine'])){
+                            $valid = true;
+                        }
+                        else{
+                            $valid = false;
+                        }
+                    }
+                    if(isset($_POST['descrizioneAnimale']) && $valid==true){
+                        if(modulesInit::validDescription($_POST['descrizioneAnimale'])){
+                            $valid = true;
+                        }
+                        else{
+                            $valid = false;
+                        }
+                    }
+                    if(isset($_FILES["immagineAnimale"]["tmp_name"]) && $valid==true){
+                        if(modulesInit::validImage($_FILES["immagineAnimale"]["tmp_name"])){
+                            $valid = true;
+                        }
+                        else{
+                            $valid = false;
+                        }
+                    }
+                    if($valid==true){
+                        $oggettoPagina = new sqlInteractions();
+                        $connessione = $oggettoPagina->apriConnessioneDB();
+                        if($connessione){
+                            $inserimento = $oggettoPagina->insertAnimal();
+                            if($inserimento){
+                                $nuovoAnimale = "Inserimento nuovo animale avvenuto con successo.";
                             }
                             else{
-                                $nuovoAnimale = "La famiglia inserita non &egrave; valido&comma; pu&ograve; contenere solo lettere e spazi&comma; riprova&period;";
+                                $nuovoAnimale = "Inserimento nuovo animale non avvenuto per un problema del database.";
                             }
                         }
                         else{
-                            $nuovoAnimale = "Non è possibile procedere all&apos;inserimento del nuovo animale perch&egrave; non sono presenti tutti i cambi obbligatori&comma; inseriscili e riprova&period;";
+                            $nuovoAnimale = "Connessione al database degli animali fallita. Non &egrave; possibile procedere con l&apos;inserimento, riprova pi&ugrave; tardi.";
                         }
                     }
                     else{
-                        $nuovoAnimale = "Il nome scientifico inserito non &egrave; valido&comma; pu&ograve; contenere solo lettere e spazi&comma; riprova&period;";
+                        $nuovoAnimale = "Le informazioni inserite non sono valide. Riprova.<br />Ricorda che i nomi possono contentere solo lettere e spazi.";
                     }
                 }
                 else{
-                    $nuovoAnimale = "Non è possibile procedere all&apos;inserimento del nuovo animale perch&egrave; non sono presenti tutti i cambi obbligatori&comma; inseriscili e riprova&period;";
+                    $nuovoAnimale = "Il nome comune e/o il nome scientifico inseriti non sono validi. Riprova.<br />Ricorda che possono contentere solo lettere e spazi.";
                 }
             }
             else{
-                $nuovoAnimale = "Il nome comune inserito non &egrave; valido&comma; pu&ograve; contenere solo lettere e spazi&comma; riprova&period;";
+                $nuovoAnimale = "Non &egrave; possibile procedere all&apos;inserimento del nuovo animale perch&egrave; non sono presenti tutti i cambi obbligatori.<br />Verifica di averli inseriti e riprova.";
             }
-            $output = str_replace("<inserimentoNuovoAnimale></inserimentoNuovoAnimale>","<p class\"messaggio\">".$nuovoAnimale."</p>",$output);
         }
+        $output = str_replace("<inserimentoNuovoAnimale></inserimentoNuovoAnimale>","<p class\"messaggio\">".$nuovoAnimale."</p>",$output);
         echo $output;
     }
 
