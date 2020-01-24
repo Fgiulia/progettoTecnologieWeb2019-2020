b9bf720e8ae25790288fe0eb372b8c8f1d576f36
TheJack95 <giacomocorro@gmail.com>
2020-01-24 15:22:51 +0100

    mi ero dimenticato in redirect

--- PAGES/paginaVuota.php
+++ PAGES/paginaVuota.php
@@ -17,11 +17,14 @@
 
     echo $output;
     if(isset($_SESSION["redirect"])){
-      if($_SESSION["redirect"] == "login"){
-      }
-      if($_SESSION["redirect"] == "areaPrivata"){
-      }
+        if($_SESSION["redirect"] == "login"){
+            header("refresh:5; url= http://localhost:8080/progettoTecnologieWeb2019-2020/PAGES/login.php");
+        }
+        if($_SESSION["redirect"] == "areaPrivata"){
+            header("refresh:5; url= http://localhost:8080/progettoTecnologieWeb2019-2020/PAGES/areaPrivata.php");
+        }
     }
     else{
+        header("refresh:5; url= http://localhost:8080/progettoTecnologieWeb2019-2020/PAGES/home.php");
     }
 ?>
