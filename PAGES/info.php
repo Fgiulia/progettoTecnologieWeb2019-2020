a596efd5f9f3a3c94fcb4615cc761e1ccf5d079c
Erik Tabacchi <tabacchi.erik@gmail.com>
2020-01-25 15:22:31 +0100

    fix invioMessaggio

--- PAGES/info.php
+++ PAGES/info.php
@@ -7,7 +7,10 @@ $output = file_get_contents("../HTML/info.html");
 $output = str_replace('<a href="info.php">','</a>',$output);
 $output = str_replace("<menu></menu>",modulesInit::menu(),$output);
 $output = str_replace("<breadcrumb></breadcrumb>",modulesInit::breadcrumb("Informazioni"),$output);
-
+if(isset($_SESSION['response'])){
+$output = str_replace("<risultato></risultato>",$_SESSION['response']->response,$output);
+unset($_SESSION['response']);
+}
 echo $output;
 
 
